4163dd11eedacbfb612cbddfab26d8239c28e355
iaramaia09 <135679521+iaramaia09@users.noreply.github.com>
2023-09-15 09:56:42 -0300

    Add files via upload

---
+++ aula_json/script.js
@@ -0,0 +1,34 @@
+const campoLogin = document.getElementById("login")
+const campoSenha = document.getElementById("password")
+const campoNovoLogin = document.getElementById("NovoLogin")
+const campoNovaSenha = document.getElementById("NovaSenha")
+const campoRepSenha = document.getElementById("RepSenha")
+let usuarios = [];
+
+function login() {
+    let login = campoLogin.value;
+    let senha = campoSenha.value;
+    let mensagem = "Usuario ou senha incorreto";
+    for (let usuario of usuarios) {
+        if (usuario.login == login && usuario.senha == senha) {
+            mensagem = "Parabéns, você está logado";
+            break;
+        }
+    }
+    alert(mensagem)
+}
+function cadastrar() {
+    if (campoNovaSenha.value == campoRepSenha.value) {
+        const usuario = {
+            login: campoNovoLogin.value,
+            senha: campoNovaSenha.value
+        }
+        usuarios.push(usuario);
+        alert("Cadastrado com sucesso!")
+    } else {
+        alert("As senhas são diferentes!")
+    }
+
+
+
+}
